2851638abd5b35ebeecfb01b9edc92732c988868
Max Sevenig <max.sevenig@hotmail.com>
2020-11-10 12:18:51 +0000

    Added seed for fake gardens

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Deleting all gardens'
+Garden.delete_all
+puts 'Creating fake gardens'
+50.times do
+  garden = Garden.new(
+    name: Faker::Restaurant.name,
+    country: Faker::Address.country,
+    description: Faker::Restaurant.description,
+    price_per_month: rand(50..300),
+    user_id: 1
+  )
+  garden.save!
+end
+puts 'Done!!'
